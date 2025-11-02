e5bac8b81aea293734e445e388f6b8a817f93986
Potato3852 <kyzmin200647@gmail.com>
2025-11-02 21:44:02 +0300

    add file support and example

--- source/main.c
+++ source/main.c
@@ -1,24 +1,37 @@
 #include "cpu.h"
 #include "assembler.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <file.asm>\n", argv[0]);
+        return 1;
+    }
+    
+    // Open file
+    FILE* file = fopen(argv[1], "r");
+    if (!file) {
+        printf("Error: Cannot open file %s\n", argv[1]);
+        return 1;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    char* program = malloc(size + 1);
+    fread(program, 1, size, file);
+    program[size] = '\0';
+    fclose(file);
+
+    // Run program
+        
     struct CPU cpu;
     cpu_init(&cpu);
     
-    const char* program = 
-        "LDI R0, 0\n"
-        "LDI R1, 1\n"
-        "LDI R2, 6\n"
-        "LDI R3, 1\n"
-        "sum_loop:\n"
-        "ADD R0, R1\n"
-        "INC R1\n"
-        "CMP R1, R2\n"
-        "JNZ sum_loop\n"
-        "HLT\n";
-    
     assemble(&cpu, program, 0x0000);
+    free(program);
     
     cpu.PC = 0x0000;
     cpu_run(&cpu);
